ef61218249eaaa2a17d9eaf8f55e8c545cc48ad3
mike <mikecee0815@gmail.com>
2018-07-15 01:22:36 -0400

    fetching data from database done

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    $links = \App\Link::all();
+
+    return view('welcome', ['links' => $links]);
+});
