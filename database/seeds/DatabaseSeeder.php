fbc57b4333b4ca46c93221856127e51a230289c7
mike <mikecee0815@gmail.com>
2018-07-15 00:59:43 -0400

    done seeding links Table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	$this->call(LinksTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
